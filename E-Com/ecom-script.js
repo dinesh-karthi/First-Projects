e753736bbb2b3e2d29cd7e38c1260f01b987ba05
dinesh003 <eshdin475@gmail.com>
2025-10-13 22:59:55 +0530

    new file data.json added and new resonsive styles added in css file and fetch api function added in javascript

--- E-Com/ecom-script.js
+++ E-Com/ecom-script.js
@@ -1,6 +1,26 @@
 
-const array =['apple','banana','icecream','mango','grape','orange']
-array.forEach((e)=> {
-    console.log(e.length>6?e:false);
+// const array =['apple','banana','icecream','mango','grape','orange']
+// array.forEach((e)=> {
+//     console.log(e.length>6?e:false);
     
+// });
+
+// selecting elements
+const title = document.querySelectorAll('.title a');
+console.log(title);
+const imgBox = document.querySelectorAll('.img-box img');
+console.log(imgBox);
+
+title.forEach((t)=>{
+    t.addEventListener('click',(e)=>{
+        e.preventDefault();
+        const api = fetch('data.json');
+        api.then((res)=>{ res.json().then((data)=>{
+            console.log(data)
+            imgBox.forEach((img,i)=>{
+                img.src = data[i].img;
+            })
+        })})
+        
+    });
 });
