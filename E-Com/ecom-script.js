a792050aa88f578268455719040bc369470edbb9
dinesh003 <eshdin475@gmail.com>
2025-10-14 16:24:59 +0530

    Fetch API added successfully for remaining products

--- E-Com/ecom-script.js
+++ E-Com/ecom-script.js
@@ -9,18 +9,22 @@
 const title = document.querySelectorAll('.title a');
 console.log(title);
 const imgBox = document.querySelectorAll('.img-box img');
-console.log(imgBox);
 
 title.forEach((t)=>{
     t.addEventListener('click',(e)=>{
         e.preventDefault();
         const api = fetch('data.json');
         api.then((res)=>{ res.json().then((data)=>{
-            console.log(data)
-            imgBox.forEach((img,i)=>{
-                img.src = data[i].img;
-            })
+            console.log(data[0]);
+            // var SRC = [data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9]]
+            // console.log(SRC)
+            
+            for(var i=0;i<imgBox.length;i++){
+                imgBox[i].src = data[i];
+                console.log(data[i]);
+            }
         })})
+        .catch((err)=>{console.log(err);})
         
     });
 });
