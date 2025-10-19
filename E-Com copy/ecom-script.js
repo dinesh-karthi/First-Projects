8caacc94072df022a52d5d4d1a4b50037d4b8dd3
dinesh003 <eshdin475@gmail.com>
2025-10-19 11:45:53 +0530

    new changes committed

---
+++ E-Com copy/ecom-script.js
@@ -0,0 +1,94 @@
+// selecting bar, sidebar
+const bar = document.querySelector('.bar')
+const sidebar = document.querySelector('.sidebar')
+
+bar.addEventListener('click',dropNavSide)
+
+function dropNavSide(){
+    sidebar.classList.add('sidebar-drop')
+  
+}
+//selecting navbar elements
+const input = document.getElementById('nav-input')
+const autoAnchor = document.querySelectorAll('.drop a')
+
+function storeValue(event){
+var key = event.key;
+if( key == 'Enter' ){
+    autoAnchor[0].textContent = input.value
+}
+
+}
+
+
+
+// selecting title navbar elements
+const title = document.querySelectorAll('.title a');
+console.log(title);
+// selecting img-boxes
+const Clothing = document.querySelectorAll('.img-box img');
+const Accesories = document.querySelectorAll('.img-box img');
+const HomeAppliances = document.querySelectorAll('.img-box img');
+const Sale = document.querySelectorAll('.img-box img');
+
+//  imgBox[0].src = "https://picsum.photos/id/4/220/220";
+
+    title[1].addEventListener('click',(e)=>{
+        e.preventDefault();
+        const api = fetch('data.json');
+        api.then((res)=>{ res.json().then((data)=>{
+            console.log(data['Clothing']);
+           
+            for(var i=0;i<Clothing.length;i++){
+                Clothing[i].src = `${data['Clothing'][i]['src'] }`;
+                console.log(data['Clothing'][i]['src']);
+            }
+        })})
+        .catch((err)=>{console.log(err);})
+        return i;
+    });
+// Accesories
+ title[2].addEventListener('click',(e)=>{
+        e.preventDefault();
+        const api = fetch('data.json');
+        api.then((res)=>{ res.json().then((data)=>{
+            console.log(data['Accesories']);
+           
+            for(var i=0;i<Accesories.length;i++){
+                Accesories[i].src = `${data['Accesories'][i]['src'] }`;
+                console.log(data['Accesories'][i]['src']);
+            }
+        })})
+        .catch((err)=>{console.log(err);})
+        
+    });
+// HomeAppliances
+    title[3].addEventListener('click',(e)=>{
+            e.preventDefault();
+            const api = fetch('data.json');
+            api.then((res)=>{ res.json().then((data)=>{
+                console.log(data['HomeAppliances']);
+            
+                for(var i=0;i<HomeAppliances.length;i++){
+                    HomeAppliances[i].src = `${data['HomeAppliances'][i]['src'] }`;
+                    console.log(data['HomeAppliances'][i]['src']);
+                }
+            })})
+            .catch((err)=>{console.log(err);})
+            
+        });
+    // Sale
+    title[4].addEventListener('click',(e)=>{
+            e.preventDefault();
+            const api = fetch('data.json');
+            api.then((res)=>{ res.json().then((data)=>{
+                console.log(data['Sale']);
+            
+                for(var i=0;i<Sale.length;i++){
+                    Sale[i].src = `${data['Sale'][i]['src'] }`;
+                    console.log(data['Sale'][i]['src']);
+                }
+            })})
+            .catch((err)=>{console.log(err);})
+            
+        });
